d8089828c19574962fd415a77485a0ec34514d33
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-04-01 23:20:17 +0530

    Update 4. Longest Subarray With Sum K.java

--- 004. Arrays/4. Longest Subarray With Sum K.java
+++ 004. Arrays/4. Longest Subarray With Sum K.java
@@ -1,3 +1,9 @@
+Q: Find the largest subarray sum 
+input : N = 8
+        A[] = {15,-2,2,-8,1,7,10,23}
+output : 5
+*********************************************************
+//Code:
 import java.util.*;
 class GfG
 {
@@ -15,5 +21,31 @@ class GfG
     }
 }
 // This taken O(N^2) which gives TLE
-
+*********************************************************************
+// Optimized a bit hard to get that one 
+// HashMap dry run it
+import java.util.*;
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+      HashMap<Integer,Integer> map=new HashMap<>();
+      int sum=0,max_len=0;
+      for(int i=0;i<n;i++){
+          sum+=arr[i];
+          if(sum==0) 
+          max_len=Math.max(max_len,i+1);
+        else {
+              if(map.containsKey(sum)){
+                max_len=Math.max(max_len,i-map.get(sum));
+              }
+              else{
+                  map.put(sum,i);
+              }
+              
+          }
+      }
+    return max_len;
+    }
+}
    
