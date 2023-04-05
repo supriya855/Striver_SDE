d469aac7a56a2f9da5ee5f2eaca5e67d022739dd
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-04-05 08:47:55 +0530

    Update 4. Check if a LinkedList is palindrome or not.java

--- 007. LinkedList and Arrays/4. Check if a LinkedList is palindrome or not.java
+++ 007. LinkedList and Arrays/4. Check if a LinkedList is palindrome or not.java
@@ -35,3 +35,50 @@ return true;
         return prev;
     }
 }
+// This is something my own approach (I never Had an idea of slow and pointer intillay)
+// First take head copy the head one variable
+// reverse the head 
+// orignal values of head and reverse values of a list
+// if both are not equal at any point of time return false else return true
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        ListNode dummy=head;
+        ListNode temp=copy(head);
+        ListNode rev=reverse(dummy);
+        // checking isPalindrome or not
+        while(temp!=null&&rev!=null){
+            if(temp.val!=rev.val){
+                return false;
+            }
+            temp=temp.next;
+            rev=rev.next;
+        }
+        return true;
+    }
+    // reverse a list
+    public ListNode reverse(ListNode h){
+        if(h==null) return h;
+        ListNode prev=null;
+        while(h!=null){
+            ListNode next=h.next;
+            h.next=prev;
+            prev=h;
+            h=next;
+        }
+        return prev;
+    }
+    //copy the original node
+    public ListNode copy(ListNode head){
+        if(head==null) return head;
+        ListNode dummy = new ListNode(-1);
+    ListNode cur = dummy;
+    while (head != null) {
+        cur.next = new ListNode(head.val);
+        cur = cur.next;
+        head = head.next;
+    }
+    return dummy.next;
+    }
+}
+
