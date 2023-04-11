1e9a38c2dd52c153a9afe3021fe8e602f8ddd27f
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-04-11 23:59:51 +0530

    Create Single Element in a Sorted Array.java

---
+++ 011. Binary Search/Single Element in a Sorted Array.java
@@ -0,0 +1,13 @@
+// Brute Force Approach
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        for(int i=0;i<n-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[n-1]!=nums[n-2]? nums[n-1]:-1; 
+    }
+}
